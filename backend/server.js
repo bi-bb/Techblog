34b66bd43756ff3031acfa69737f41408a91428c
bi <bi@bis-Mac.local>
2026-04-05 20:13:53 +1000

    Update project files

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
-const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const postRoutes = require("./routes/postRoutes");
-
-
-dotenv.config();
-connectDB();
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -23,7 +20,14 @@ app.use("/api/posts", postRoutes);
 
 const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
